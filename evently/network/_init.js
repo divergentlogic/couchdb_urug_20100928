7877997bd15b3fcf434f45d63ff8243c28685423
Christopher Durtschi <pherph@gmail.com>
2010-10-03 16:35:21 -0600

    Remove unused network event

--- evently/network/_init.js
+++
@@ -1,9 +0,0 @@
-function () {
-  var elem = $(this);
-  $("window").bind("online", function () {
-    elem.trigger("online");
-  });
-  $("window").bind("offline", function () {
-    elem.trigger("offline");
-  });
-}
